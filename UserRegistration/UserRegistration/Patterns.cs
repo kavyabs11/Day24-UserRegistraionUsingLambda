910dfc59f80b1042ee3ed730de70a9bfe37af905
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-14 23:32:51 +0530

    By using rules

--- UserRegistration/UserRegistration/Patterns.cs
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -9,7 +9,8 @@ namespace UserRegistration
 {
     public class Patterns
     {
-        public static string email = "^[a-zA-Z0-9]+([!@#$%^&*()_+.,][a-zA-Z0-9]+)*[@][a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$";
+
+        public static string email = "^[a-zA-Z0-9]+([!@#$%^&*()_+.,-][a-zA-Z0-9]+)*[@][a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$";
         public void Validation(string sample)
         {
             if (Regex.IsMatch(sample, email))
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -2,6 +2,22 @@
 
 Console.WriteLine("Welcome to User Registration program using Regex patterns");
 Patterns patterns = new Patterns();
-Console.WriteLine("Enter the password to create");
-string password = Console.ReadLine();
-patterns.Validation(password);
+Console.WriteLine("Checking the sample email address");
+string email1 = "abc@yahoo.com"; //valid
+patterns.Validation(email1);
+string email2 = "abc-100@yahoo.com"; //valid
+patterns.Validation(email2);
+string email3 = "abc.100@yahoo.com"; //valid
+patterns.Validation(email3);
+string email4 = "abc111@abc.com"; //valid
+patterns.Validation(email4);
+string email5 = "abc-100@abc.net"; //valid
+patterns.Validation(email5);
+string email6 = "abc.100@abc.com.au"; //valid
+patterns.Validation(email6);
+string email7 = "abc@1.com"; //invalid
+patterns.Validation(email7);
+string email8 = "abc@gmail.com.com"; //valid
+patterns.Validation(email8);
+string email9 = "abc+100@gmail.com"; //valid
+patterns.Validation(email9);
