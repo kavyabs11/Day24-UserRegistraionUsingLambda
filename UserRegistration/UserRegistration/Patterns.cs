c7062d895594a3c8789d85b1c6339290ab66acac
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-14 23:11:54 +0530

    By using rules

---
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Patterns
+    {
+        public static string rules = "^[A-Z][a-z]{3,}";
+        public bool Validation(string sample)
+        {
+            return Regex.IsMatch(sample, rules);
+        }
+    }
+}
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace UserRegistration
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration program using Regex patterns");
+Patterns patterns = new Patterns();
+Console.WriteLine("Enter the First name");
+string n = Console.ReadLine();
+if (patterns.Validation(n))
+    Console.WriteLine("Valid first name");
+else
+    Console.WriteLine("Invalid first name");
