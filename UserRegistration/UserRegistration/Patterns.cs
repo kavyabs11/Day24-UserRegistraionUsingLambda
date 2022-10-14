854f4662180542964f49e0f4f70436b13ba4af62
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-14 23:17:44 +0530

    By using rules

--- UserRegistration/UserRegistration/Patterns.cs
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -9,13 +9,13 @@ namespace UserRegistration
 {
     public class Patterns
     {
-        public static string lastname = "^[A-Z][a-z]{2,}";
+        public static string email = "^[a-zA-Z0-9]+([!@#$%^&*()_+.,][a-zA-Z0-9]+)*[@][a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
         public void Validation(string sample)
         {
-            if (Regex.IsMatch(sample, lastname))
-                Console.WriteLine("Valid lastname");
+            if (Regex.IsMatch(sample, email))
+                Console.WriteLine("Valid email address");
             else
-                Console.WriteLine("Invalid lastname");
+                Console.WriteLine("Invalid email address");
         }
     }
 }
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -2,6 +2,6 @@
 
 Console.WriteLine("Welcome to User Registration program using Regex patterns");
 Patterns patterns = new Patterns();
-Console.WriteLine("Enter the Last name");
-string lastname = Console.ReadLine();
-patterns.Validation(lastname);
+Console.WriteLine("Enter the email address");
+string email = Console.ReadLine();
+patterns.Validation(email);
